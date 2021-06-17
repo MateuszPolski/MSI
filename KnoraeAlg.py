1d3248b283dfb6423418ea2905b7223867bcf158
MateuszPolski <81154144+MateuszPolski@users.noreply.github.com>
2021-06-17 18:45:00 +0200

    akutualizajcja
    kekw

--- KnoraeAlg.py
+++ KnoraeAlg.py
@@ -2,21 +2,122 @@ import numpy as np
 from sklearn.ensemble import BaseEnsemble
 from sklearn.base import ClassifierMixin, clone
 from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
-
+from sklearn.neighbors import NearestNeighbors
+from sklearn.base import clone
+from sklearn.metrics import accuracy_score
 
 class KnoraeAlg(BaseEnsemble,ClassifierMixin):
-
-    def __init__(self, base_estimator=None, n_estimators=5, hard_voting=True, random_state=None):
-        self.base_estimator = base_estimator
-        self.n_estimators = n_estimators
-        self.hard_voting = hard_voting
+    # zdefinioweanie __init__
+    def __init__(self, pool_classifiers=None, random_state=None,k=5):
+        #okreslenie zbioru wykorzystywanych klasyfikatorow
+        self.pool_classifiers = pool_classifiers
+        #okreslenie wielkosci obszaru kompetencji
+        self.k=k
+        #okreslenie ziarna losowosci
         self.random_state = random_state
         np.random.seed(self.random_state)
 
     def fit(self,X,y):
+        #sprawdzenie zbiorow X oraz y
         X, y = check_X_y(X, y)
-        self.classes_ = np.unique(y)
+        #przechowujemy X i y
 
+        self.X_, self.y_ = X, y
+        #okreslenie zespolu klasyfikatorow przez przypisanie in nazw do tablicy ensemble_
+        self.ensemble_ = []
+        #przypisanie klasyfikatorow ze zbioru pool_classifiers do tablicy ensemble_
+        for i in range(len(self.pool_classifiers)):
+            self.ensemble_.append(clone(self.pool_classifiers[i]).fit(self.X_, self.y_))
+
+        return self
 
     def predict(self,X):
-        check_is_fitted(self, "classes_")
+        #wyznacznie najblizszych sasiadow przy pomocy algorytmu NearestNeighbors
+        #n_neighbors- ile nablizszych sasiadow ma wyznaczyc jest rowne wielkosci obszaru kompetencji k
+        #algorithm- jaki algorytm bedzie wykorzystany w NearestNeighbors
+        nbrs = NearestNeighbors(n_neighbors=self.k, algorithm='ball_tree').fit(self.X_)
+        #okreslanie sasiadow naszych sampli
+        distances, indices = nbrs.kneighbors(X)
+        #inicjacja macierzy przechowujacej dokladnosci poszegolnych klasyfiaktow w obszaru kompetencji k
+        scores = np.zeros((len(self.pool_classifiers), indices.shape[0]))
+        #inicjacja macierzy przechowujacej informacje o klasyfiaktorach ktore osiagnely 100% dokladnosci w obszaru kompetencji k
+        check_oracle=np.zeros((len(self.pool_classifiers), indices.shape[0]))
+
+        #zmienna k do dzialania petli while
+        k=0
+        while True:
+            #tablica do ktorej beda przypisywane wzorce dla ktroych zaden klasyfiaktor nie osiagnal 100 dokladnosci
+            no_100 = []
+            # zatrzymanie petli gdy region kompetecji bedzie rowny 0
+            if indices.shape[1] == 0:
+                break
+            #okreslanie klasyfikatorow ktore osiagnely 100 dokladnosci w regionie kompetecji
+            for j in range(indices.shape[0]):
+                #kopia zbiorow X oraz y
+                temp_X=self.X_
+                temp_y=self.y_
+                #przypisanie sasiadow probki dla ktorych dokonujemy predykcji w regionie kompetecji
+                test=[]
+                test=indices[j]
+                #usuniecie probek testowych ze zbioru uczacego
+                train_X=np.delete(temp_X,test,0)
+                train_y=np.delete(temp_y,test,0)
+                #lista do ktorej przypisujemy wyniki predykcji dla sasiadow danej probki
+                temporary = []
+                #dokonanie predykcji dla sasiadow danej probki
+                for i in range(len(self.pool_classifiers)):
+                    #okreslanie klasyfikatora
+                    clf=self.pool_classifiers[i]
+                    #dopasowanie modelu
+                    clf.fit(train_X, train_y)
+                    #dokonanie predykcji
+                    y_pred=clf.predict(temp_X[test])
+                    #sprawdzenie czy jakis klasyfikator osiagnal 100 dokladnosci po pierwszej iteracji
+                    for z in range(len(self.pool_classifiers)):
+                        if scores[z][j] == 1:
+                            break
+                    #wpisanie dokladnosci jaka uzyskal dany klasyfiaktor
+                    scores[i][j]=accuracy_score(temp_y[test],y_pred)
+                    #przypisanie wyników gdy dokladnosc osiagnela 100% do check_oracle
+                    if 1.0 == scores[i][j]:
+                        check_oracle[i][j]=1
+                    temporary.append(accuracy_score(temp_y[test],y_pred))
+                #przypisanie numerow probek dla ktroych nie osiagnieto 100 dokladnosci w ich regionie
+                if not 1.0 in temporary:
+                    no_100.append(j)
+            #jezeli dla wszysztkich regionow osiagnieto 100 dokladnosci
+            if len(no_100) == 0:
+                break
+            k+=1
+            #pomniejszenie regionu kompetecji
+            indices=np.delete(indices,indices.shape[1]-1,axis=1)
+
+        #zmiana wartosci float na int w macierzy chceck_oracle
+        check_oracle = check_oracle.astype(int)
+        #inicjacja macierzy zawięrającej głosy poszczególnych klasyfikatorów w zespole
+        pred_=np.empty([check_oracle.T.shape[0],len(self.pool_classifiers)],dtype=np.uint8)
+
+
+        X_=[]
+
+        # w tej pętli dokonuje się predykcji poszczególnych wzorców
+        for j in range(X.shape[0]):
+            #zmiana wymiaru z 1D do 2D
+            X_= X[j].reshape(1,-1)
+            #pętla, w której wyznaczone klasyfikatory dokonują predykcji
+            for i,member_clf in enumerate(self.ensemble_):
+                #jeżeli żaden klasyfikator nie osiągnął 100 % dokładności predykcji dokonuje pierwszy w zespole
+                if np.max(check_oracle.T[j]) == 0:
+                    pred_[j]=member_clf.predict(X_)
+                    break
+                #jeżeli dany klasyfikator jest upoważniony do podjęcia predykji jej wynik zapisywany jest w macierzy pred_
+                elif check_oracle.T[j][i] == 1:
+                    pred_[j][i] = member_clf.predict(X_)
+                # po predykcji wychodzimy z pętli wewnętrznej i przechodzimy do kolejnego wzorca
+                else:
+                    continue
+        #liczenie głosów w obrębie jednego zespołu
+        prediction = np.apply_along_axis(lambda x: np.argmax(np.bincount(x)), axis=1, arr=pred_)
+        #zwracamy macierz z wynikami predykcji
+        return prediction
+
--- PROJEKT.py
+++ PROJEKT.py
@@ -6,93 +6,113 @@ from deslib.des.knora_u import KNORAU
 from deslib.des.des_knn import DESKNN
 from sklearn.base import clone
 from sklearn.ensemble import AdaBoostClassifier
-from scipy.stats import rankdata
-#from scipy.stats import ttest_rel
-from scipy.stats import ranksums
 from tabulate import tabulate
 from KnoraeAlg import KnoraeAlg
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from scipy.stats import ttest_rel
 
+#lista z nazwami wybranymi zbiorami danych
+datasets =['fri_c0_100_25','chscase_census4','breastcancoimbra','cryotherapy','chscase_census5','chscase_census3','heart','chscase_census2','sonar','fri_c0_500_10','fri_c4_500_50','pwLinear','phpAyyBys','fri_c3_100_5','fri_c1_100_5','ex2data2','phphZierv','visualizing_environmental','fri_c4_100_25','fri_c0_100_5']
 
-
-datasets =['banknote','breastcan','breastcancoimbra','cryotherapy','diabetes','german','heart','ionosphere','sonar','spambase','wine','wisconsin','phpAyyBys','dataset_net','fri_c1_100_5','ex2data2','phphZierv','visualizing_environmental','fri_c4_100_25','fri_c0_100_5']
-
-
+#określenie długości listy zawierającej zbiory danych
 n_datasets = len(datasets)
+#ilość foldów
 n_splits = 5
+#ilość powtórzeń
 n_repeats = 10
-rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=1234)
+#inicjalizacja stratyfikowanej walidacji krzyżowej
+rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=2137)
 
 
-
+#inicjalizacja zespołów klasyfikatorów
 clfs= {
     'KNORAU':KNORAU(random_state=2137),
+    'KNORAE': KnoraeAlg(pool_classifiers=[GaussianNB(),DecisionTreeClassifier(random_state=2137),KNeighborsClassifier()]),
     'DESKNN':DESKNN(random_state=2137),
     'ADABOOST':AdaBoostClassifier(base_estimator=GaussianNB(),random_state=2137),
+
 }
+#inicjalizacja nagłówków do tabel
+headers = ["KNORAU", "KNORAE", "DESKNN","ADABOOST"]
+#inicjalizacja nazw kolumn do tabel
+names_column = np.array([["KNORAU"], ["KNORAE"], ["DESKNN"],["ADABOOST"]])
 
-scores = np.zeros((len(clfs), n_datasets  ,n_splits * n_repeats))
-'''
+
+# pętla wyznaczająca dokładności poszczególnych zespołów dla konkretnych zbiorów danych
 for data_id, dataset in enumerate(datasets):
+    #wyświetlenie nazwy zbioru danych
+    print("Zbiór danych:",dataset)
+    #inicjalizacja macierzy na wyniki
+    scores = np.zeros((len(clfs), n_splits * n_repeats))
+    #wyodrębnienie zbioru danych z pliku
     dataset = np.genfromtxt("%s.csv" % (dataset), delimiter=",")
+    #określenie wzoroców z atrybutami
     X = dataset[:, :-1]
+    #wyodrębnienie etykiet
     y = dataset[:, -1].astype(int)
+    #wyświetlenie liczby wzorców
+    print("Liczba wzorców:",X.shape[0])
+    #wyświetlenie liczby atrybutów
+    print("Liczba atrybutów:",X.shape[1])
+    #wyznacznie unikalnych wartości ze zbioru etykiet
+    values, counts = np.unique(y, return_counts=True)
+    #wyświetlenie klas
+    print("Jakie klasy:",values)
+    #wyświetlenie liczby wzorców w danej klasie
+    print("Liczność klas",counts)
+    #pętla do wyznaczenie dokładkości poszczególnych zespołów wraz z stratyfikowaną walidacją krzyżową
     for fold_id, (train, test) in enumerate(rskf.split(X, y)):
+    #dla poszczególnych zespołów
         for clf_id, clf_name in enumerate(clfs):
+            #klonowanie zespołów
             clf = clone(clfs[clf_name])
+            #dopasowanie danych
             clf.fit(X[train], y[train])
+            #predykcja
             y_pred = clf.predict(X[test])
-            scores[clf_id,data_id, fold_id] = accuracy_score(y[test], y_pred)
+            #wyliczenie dokładności dla każdego folda
+            scores[clf_id, fold_id] = accuracy_score(y[test], y_pred)
+    #obliczenie średniej dokładoności
+    mean = np.mean(scores, axis=1)
+    #obliczenie odchylenia standardowego
+    std = np.std(scores, axis=1)
+    #wyświetlenie średnich dla każdego z zespołów
+    print("średnie dla danych zespołów")
+    for clf_id, clf_name in enumerate(clfs):
+        print("%s: %.3f (%.2f)" % (clf_name, mean[clf_id], std[clf_id]))
+    #określenie poziomu ufności do testów statystycznych
+    alfa = .05
+    #inicjalizacja macierzy na t-statystyki
+    t_statistic = np.zeros((len(clfs), len(clfs)))
+    #inicjalizacja macierzy na p-wartości
+    p_value = np.zeros((len(clfs), len(clfs)))
 
-np.save('results', scores)
-'''
+    #obliczenie t-statystyki oraz p-wartości
+    for i in range(len(clfs)):
+        for j in range(len(clfs)):
+            t_statistic[i, j], p_value[i, j] = ttest_rel(scores[i], scores[j])
 
-scores = np.load('results.npy')
-mean_scores = np.mean(scores, axis=2).T
-print("\nMean scores:\n", mean_scores)
+    #wyświetlenie sformatowanej tabeli z t-statystykami i p-wartościami
+    t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
+    t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
+    p_value_table = np.concatenate((names_column, p_value), axis=1)
+    p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+    print("t-statystyka:\n", t_statistic_table, "\n\np-wartość:\n", p_value_table)
 
-mean_scores_1 = np.mean(mean_scores, axis=0)
-print("\nMean ranks:\n", mean_scores_1)
+    #wyświetlenie sformatowanej tabeli przewag
 
-ranks = []
-for ms in mean_scores:
-    ranks.append(rankdata(ms).tolist())
-ranks = np.array(ranks)
+    advantage = np.zeros((len(clfs), len(clfs)))
+    advantage[t_statistic > 0] = 1
+    advantage_table = tabulate(np.concatenate(
+        (names_column, advantage), axis=1), headers)
+    print("Przewagi:\n", advantage_table)
 
+    #wyświetlenie zformatowanej tabeli zależności statystycznych
 
+    significance = np.zeros((len(clfs), len(clfs)))
+    significance[p_value <= alfa] = 1
+    significance_table = tabulate(np.concatenate(
+        (names_column, significance), axis=1), headers)
+    print("Zależności statystyczne (alpha = 0.05):\n", significance_table)
 
-mean_ranks = np.mean(ranks, axis=0)
-print("\nMean ranks:\n", mean_ranks)
-
-
-alfa = .05
-w_statistic = np.zeros((len(clfs), len(clfs)))
-p_value = np.zeros((len(clfs), len(clfs)))
-
-for i in range(len(clfs)):
-    for j in range(len(clfs)):
-        w_statistic[i, j], p_value[i, j] = ranksums(ranks.T[i], ranks.T[j])
-
-alfa = .05
-t_statistic = np.zeros((len(clfs), len(clfs)))
-p_value = np.zeros((len(clfs), len(clfs)))
-
-for i in range(len(clfs)):
-    for j in range(len(clfs)):
-        t_statistic[i, j], p_value[i, j] = ttest_rel(ranks.T[i], ranks.T[j])
-print("t-statistic:\n", t_statistic, "\n\np-value:\n", p_value)
-
-
-
-headers = list(clfs.keys())
-names_column = np.expand_dims(np.array(list(clfs.keys())), axis=1)
-w_statistic_table = np.concatenate((names_column, w_statistic), axis=1)
-w_statistic_table = tabulate(w_statistic_table, headers, floatfmt=".2f")
-p_value_table = np.concatenate((names_column, p_value), axis=1)
-p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
-print("\nw-statistic:\n", w_statistic_table, "\n\np-value:\n", p_value_table)
-
-advantage = np.zeros((len(clfs), len(clfs)))
-advantage[w_statistic > 0] = 1
-advantage_table = tabulate(np.concatenate(
-    (names_column, advantage), axis=1), headers)
-print("\nAdvantage:\n", advantage_table)
